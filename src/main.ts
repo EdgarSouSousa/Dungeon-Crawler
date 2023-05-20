2d2ac8c710026dc2e73254f33450739df79b32d5
edgarsousousa <edgarsousa@ua.pt>
2023-05-20 11:53:45 +0100

    Added Player controller module with fps controls

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import * as THREE from 'three'
 import DungeonScene from './DungeonScene'
+import Player from './player';
+
 
 const width = window.innerWidth
 const height = window.innerHeight
+
 
 const renderer = new THREE.WebGLRenderer({
 	canvas: document.getElementById('app') as HTMLCanvasElement
@@ -10,20 +13,53 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(width, height)
 
 const mainCamera = new THREE.PerspectiveCamera(60, width / height, 0.1, 1000)
+//setupCameraControls();
 
 //set background color as clear
 renderer.setClearColor(0xffffff, 1)
 
 
-const scene = new DungeonScene(mainCamera)
-scene.initialize()
+const scene = new DungeonScene()
+await scene.initialize()
+const player = new Player({ scene , camera: mainCamera, renderer: renderer });
+await player.loadModel('/assets/rigged_arms/scene.gltf');
+player.setupControls();
+
 
 
 function tick()
 {
 	scene.update()
+    player.update();
 	renderer.render(scene, mainCamera)
 	requestAnimationFrame(tick)
 }
 
 tick()
+
+function setupCameraControls() {
+	document.addEventListener('keydown', onDocumentKeyDown, false);
+  
+	function onDocumentKeyDown(event: { keyCode: any }) {
+	  const delta = 2;
+	  const keycode = event.keyCode;
+  
+	  switch (keycode) {
+		case 37: // left arrow
+		  mainCamera.position.x -= delta;
+		  break;
+		case 38: // up arrow
+		mainCamera.position.z -= delta;
+		  break;
+		case 39: // right arrow
+		mainCamera.rotation.y += delta/100;
+		  break;
+		case 40: // down arrow
+		mainCamera.position.z += delta;
+		  break;
+	  }
+	}
+  }
+
+
+
---
+++ src/player.ts
@@ -0,0 +1,91 @@
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader';
+import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockControls';
+
+interface IPlayerOptions {
+  scene: THREE.Scene;
+  camera: THREE.Camera;
+  renderer: THREE.WebGLRenderer;
+}
+
+class Player {
+  private scene: THREE.Scene;
+  private camera: THREE.Camera;
+  private renderer: THREE.WebGLRenderer;
+  private model: THREE.Object3D | null;
+  private controls: PointerLockControls | null;
+  private keysPressed: Set<string>;
+
+  constructor({ scene, camera, renderer }: IPlayerOptions) {
+    this.scene = scene;
+    this.camera = camera;
+    this.renderer = renderer;
+    this.model = null;
+    this.controls = null;
+    this.keysPressed = new Set();
+    this.setupKeyListeners();
+  }
+
+  private setupKeyListeners(): void {
+    window.addEventListener("keydown", this.onKeyDown.bind(this));
+    window.addEventListener("keyup", this.onKeyUp.bind(this));
+  }
+
+  private onKeyDown(event: KeyboardEvent): void {
+    this.keysPressed.add(event.key);
+  }
+
+  private onKeyUp(event: KeyboardEvent): void {
+    this.keysPressed.delete(event.key);
+  }
+
+  async loadModel(url: string): Promise<void> {
+    const loader = new GLTFLoader();
+    const gltf = await loader.loadAsync(url);
+    this.model = gltf.scene;
+    this.scene.add(this.model);
+  }
+
+  setupControls(): void {
+    this.controls = new PointerLockControls(this.camera, this.renderer.domElement);
+    this.controls.getObject().position.set(0, 0, 10);
+    this.scene.add(this.controls.getObject());
+
+    // Set up the click event listener to lock the pointer
+    this.renderer.domElement.addEventListener("click", () => {
+      if (!document.pointerLockElement) {
+        this.renderer.domElement.requestPointerLock();
+      }
+    });
+  }
+
+  update(){
+    if (this.model) {
+      const moveSpeed = 0.5;
+      const direction = new THREE.Vector3();
+
+      direction.set(0, 0, 0);
+      if (this.keysPressed.has("w") || this.keysPressed.has("W")) {
+        direction.z += 1;
+      }
+      if (this.keysPressed.has("s") || this.keysPressed.has("S")) {
+        direction.z -= 1;
+      }
+      if (this.keysPressed.has("a") || this.keysPressed.has("A")) {
+        direction.x -= 1;
+      }
+      if (this.keysPressed.has("d") || this.keysPressed.has("D")) {
+        direction.x += 1;
+      }
+
+      direction.normalize();
+      direction.multiplyScalar(moveSpeed);
+      if (this.controls) {
+        this.controls.moveRight(direction.x);
+        this.controls.moveForward(direction.z);
+      } 
+    }
+  }
+}
+
+export default Player;
